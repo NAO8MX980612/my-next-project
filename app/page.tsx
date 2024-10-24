907ee6c182dad8c1b1247556420d0341c07ef4f6
NAO8MX980612 <zhizaiqiantian707@gmail.com>
2024-10-24 22:12:04 +0900

    test

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,12 @@ import styles from "./page.module.css";
 export default function Home() {
   const name= "地球";
   return (
-    <div>
-      <h1 className={styles.title}>テクノロジーの力で{name}を変える</h1>
-      <p className={styles.description}>私たちは市場をリードしているグローバルテックカンパニーです。</p>
-    </div>
+    <section className={styles.top}>
+      <div>
+        <h1 className={styles.title}>テクノロジーの力で{name}を変える</h1>
+        <p className={styles.description}>私たちは市場をリードしているグローバルテックカンパニーです。</p>
+      </div>
+      <img className={styles.bgimg} src="/img-mv.jpg" alt="" />
+    </section>
   );
 }
