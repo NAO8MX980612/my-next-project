178593f1dab11b386748a7232beaaac61f707d37
NAO8MX980612 <zhizaiqiantian707@gmail.com>
2024-10-27 00:19:44 +0900

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import styles from "./page.module.css";
 import Image from "next/image";
+import ButtonLink from "@/app/_components/ButtonLink";
 
 type News = {
   id: string;
@@ -96,6 +97,9 @@ export default function Home() {
             </li>
           ))}
         </ul>
+        <div className={styles.newsLink}>
+          <ButtonLink href="/news">もっとみる</ButtonLink>
+        </div>
       </section>
     </>
   );
