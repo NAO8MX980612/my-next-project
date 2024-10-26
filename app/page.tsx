37acf4b08a531bdf2bf40103cea5e8de31c01a48
NAO8MX980612 <zhizaiqiantian707@gmail.com>
2024-10-26 19:00:34 +0900

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,15 @@
 import styles from "./page.module.css";
+import Image from "next/image";
+
+type News = {
+  id: string;
+  title: string;
+  category: {
+    name: string;
+  };
+  publishedAt: string;
+  createdAt: string;
+}
 
 export default function Home() {
   const name= "地球";
@@ -8,7 +19,13 @@ export default function Home() {
         <h1 className={styles.title}>テクノロジーの力で{name}を変える</h1>
         <p className={styles.description}>私たちは市場をリードしているグローバルテックカンパニーです。</p>
       </div>
-      <img className={styles.bgimg} src="/img-mv.jpg" alt="" />
+      <Image 
+        className={styles.bgimg} 
+        src="/img-mv.jpg" 
+        alt="" 
+        width={4000} 
+        height={1200} 
+      />
     </section>
   );
 }
